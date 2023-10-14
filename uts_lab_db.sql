e078334b0bb3051a6cbce3559ed38607b5ff7772
Kevin Wijaya Hadinata <75928654+kevinh11@users.noreply.github.com>
2023-10-14 21:26:50 +0700

    SQL script added

---
+++ uts_lab_db.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE todo_list;
+DROP DATABASE todo_list;
+USE todo_list;
+DROP TABLE todo_items;
+CREATE TABLE todo_items (
+  task_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  task_name VARCHAR(100),
+  task_status VARCHAR(20),
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES USERS (user_id)
+);
+
+CREATE TABLE USERS(
+  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_name VARCHAR(20) NOT NULL,
+  user_pass VARCHAR(30) NOT NULL
+); 
+
+
+
+
+
