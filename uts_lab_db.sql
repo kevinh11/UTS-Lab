bef3145880c0c35401477963d0a7f01553497d16
Javier Raell <alvnaltair@gmail.com>
2023-10-22 13:21:39 +0700

    baru

--- uts_lab_db.sql
+++ uts_lab_db.sql
@@ -2,6 +2,11 @@ CREATE DATABASE todo_list;
 DROP DATABASE todo_list;
 USE todo_list;
 DROP TABLE todo_items;
+CREATE TABLE USERS(
+  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_name VARCHAR(20) NOT NULL,
+  user_pass VARCHAR(30) NOT NULL
+); 
 CREATE TABLE todo_items (
   task_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   task_name VARCHAR(100),
@@ -10,11 +15,7 @@ CREATE TABLE todo_items (
   FOREIGN KEY (user_id) REFERENCES USERS (user_id)
 );
 
-CREATE TABLE USERS(
-  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  user_name VARCHAR(20) NOT NULL,
-  user_pass VARCHAR(30) NOT NULL
-); 
+
 
 
 
