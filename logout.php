f56913c7068e1439e7701dd7d6b9ee19544692b9
kevinh11 <75928654+kevinh11@users.noreply.github.com>
2023-10-23 23:44:56 +0700

    added user id as filter for tasks

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 <?php
    setcookie('username', '', time()- 3600);
    setcookie('loggedIn', true , time()-3600);
-   setcookie('userId', $user_id, time() -3600);
+   setcookie('userId', 0, time() -3600);
 
    header("Location: login.php");
 
